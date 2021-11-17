7db1d34ce5c705984e01a43ec556a9a520bd3ea9
jakubOSSR <90446736+jakubOSSR@users.noreply.github.com>
2021-11-17 22:47:29 +0100

    Pracuje sa na tom

--- src/com/automaty/PrechodovaFunkcia.java
+++ src/com/automaty/PrechodovaFunkcia.java
@@ -1,6 +1,7 @@
 package com.automaty;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -8,11 +9,23 @@ public class PrechodovaFunkcia {
 
     private List<String> stavy;
     private List<String> symboly;
+    private String [][][] prechodovaTabulka;
 
 
-    public PrechodovaFunkcia(List<String> stavy, List<String> symboly){
-        this.stavy = stavy;
+    public PrechodovaFunkcia(List<String> stavy, List<String> symboly, String [][][] prechodovaTabulka){
+        this.stavy=stavy;
         this.symboly=symboly;
+        this.prechodovaTabulka = prechodovaTabulka;
+        System.out.println("Stavy: "+stavy);
+        System.out.println("Symboly: "+symboly);
+        System.out.println("Prechodova tabulka:\n");
+        System.out.println(Arrays.deepToString(prechodovaTabulka).replace("]]"," \n")
+                                                                 .replace("["," ")
+                                                                 .replace("]"," ")
+                                                                 .replace("null","")
+                                                                 .replace(",",""));
+
     }
+
 
 }
--- src/com/automaty/classToRemove.java
+++ src/com/automaty/classToRemove.java
@@ -8,8 +8,7 @@ public class classToRemove {
         List<String> akceptujuceStavy = new ArrayList<>();
         List<String> symboly = new ArrayList<>();
         String zaciatocnyStav = null;
-        PrechodovaFunkcia prechodova;
-        String [] [] [] prechodovaTabulka;
+        String [][][] prechodovaTabulka;
 
 
         stavy.add("q0");
@@ -19,8 +18,17 @@ public class classToRemove {
         symboly.add("1");
         akceptujuceStavy.add("q2");
         zaciatocnyStav = "q0";
-        prechodova = new PrechodovaFunkcia(stavy,symboly);
 
+        prechodovaTabulka = new String[stavy.size()][symboly.size()][symboly.size()];
+        prechodovaTabulka[0][0][0] ="q0";
+        prechodovaTabulka[0][0][1] ="q1";
+        prechodovaTabulka[0][1][0] ="q1";
+        prechodovaTabulka[1][0][0] ="q2";
+        prechodovaTabulka[1][1][0] ="q0";
+        prechodovaTabulka[2][0][0] ="q2";
+        prechodovaTabulka[2][1][0] ="q1";
+        prechodovaTabulka[2][1][1] ="q2";
+        new PrechodovaFunkcia(stavy,symboly,prechodovaTabulka);
         new NedeterministickyKonecnyAutomat(stavy,symboly,zaciatocnyStav,akceptujuceStavy);
 
 
