b253d7311912e51522a4fc369630079b2e41b4de
IvanAos <90446770+IvanAos@users.noreply.github.com>
2021-11-26 01:29:57 +0100

    Update DKAkRegularnejG.java

--- src/com/gramatiky/DKAkRegularnejG.java
+++ src/com/gramatiky/DKAkRegularnejG.java
@@ -1,5 +1,30 @@
 package com.gramatiky;
 
+import com.automaty.DeterministickyKonecnyAutomat;
+import com.automaty.Tabulka;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 public class DKAkRegularnejG {
-    
+
+    private ArrayList<String> pravastrana;
+    private LinkedHashSet<Pravidlo> pravidla;
+    public DKAkRegularnejG() throws Exception {
+        Tabulka dkakrg;
+        dkakrg = new Tabulka<>();
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
