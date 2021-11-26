faec411976076143f38e77e149578f4452b70d13
IvanAos <90446770+IvanAos@users.noreply.github.com>
2021-11-26 01:30:00 +0100

    Create Skuska.java

---
+++ src/com/gramatiky/Skuska.java
@@ -0,0 +1,44 @@
+package com.gramatiky;
+
+import org.junit.platform.engine.support.descriptor.FileSystemSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Skuska {
+    public static void main (String [] args) throws Exception {
+
+
+        HashSet<String> terminaly = new HashSet<>();
+        HashSet<String> neterminaly = new HashSet<>();
+        String zaciatocnySymbol;
+
+        terminaly.add("e");
+        terminaly.add("l");
+        terminaly.add("s");
+        terminaly.add("n");
+        terminaly.add("d");
+
+        neterminaly.add("S");
+        neterminaly.add("a");
+        neterminaly.add("B");
+        neterminaly.add("C");
+        neterminaly.add("D");
+
+        zaciatocnySymbol ="S";
+
+        Pravidlo pravidlo1 = new Pravidlo(new ArrayList<String>(Arrays.asList("S")),new ArrayList<String>(Arrays.asList("e", "A")));
+        Pravidlo pravidlo2 = new Pravidlo(new ArrayList<String>(Arrays.asList("A")),new ArrayList<String>(Arrays.asList("l", "B")));
+        Pravidlo pravidlo3 = new Pravidlo(new ArrayList<String>(Arrays.asList("A")),new ArrayList<String>(Arrays.asList("n", "C")));
+        Pravidlo pravidlo4 = new Pravidlo(new ArrayList<String>(Arrays.asList("B")),new ArrayList<String>(Arrays.asList("s", "D")));
+        Pravidlo pravidlo5 = new Pravidlo(new ArrayList<String>(Arrays.asList("C")),new ArrayList<String>(Arrays.asList("d")));
+        Pravidlo pravidlo6 = new Pravidlo(new ArrayList<String>(Arrays.asList("D")),new ArrayList<String>(Arrays.asList("e")));
+
+        LinkedHashSet<Pravidlo> pravidla = new LinkedHashSet<Pravidlo>(Arrays.asList(pravidlo1, pravidlo2, pravidlo3, pravidlo4, pravidlo5, pravidlo6));
+        RegularnaGramatika reg = new RegularnaGramatika(terminaly,neterminaly,zaciatocnySymbol,pravidla);
+
+
+    }
+}
