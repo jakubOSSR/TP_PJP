3c85959b5cc84524513308f87e44da02b420c49e
davidpnx <90611271+davidpnx@users.noreply.github.com>
2021-11-29 21:07:23 +0100

    First FINAL!

--- src/main/java/first_follow/FirstAFollow.java
+++ src/main/java/first_follow/FirstAFollow.java
@@ -1,257 +1,99 @@
 package first_follow;
-import com.gramatiky.*;
+import com.gramatiky.BezkontextovaGramatika;
+import com.gramatiky.Pravidlo;
 
-import javax.naming.event.ObjectChangeListener;
-import java.security.PrivilegedAction;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FirstAFollow {
+    public static HashMap<String, ArrayList<String>> first(BezkontextovaGramatika g) {
 
-    private HashSet<String> mnozina = new HashSet<>();
-    private HashMap<String, HashSet<String>> vysledok = new HashMap<>();
-    private HashSet<String> pomocna = new HashSet<>();
+        HashMap<String, ArrayList<String>> vysledok = new HashMap<>();
 
-    String eps = "epsilon";
-
-    public void first(BezkontextovaGramatika g) throws Exception {
-
-
-        for (String t : g.getTerminaly()) { // Prehladavanie terminalov
-            mnozina = new HashSet<>();
+        for (String t : g.getTerminaly()) {                     //Prehladavanie terminalov
+            ArrayList<String> mnozina = new ArrayList<>();
             mnozina.add(t);
-            vysledok.put(t, mnozina);      // vytvorenie mnozin first pre terminaly... First(a) = a
+            vysledok.put(t, mnozina);                           //Vytvorenie mnozin first pre terminaly... First(a) = a
         }
 
-        for (Pravidlo p : g.getPravidla()) {  //prehladavanie pravidiel
-            String symbol = p.getLavaStrana().get(0);   //do premennej "symbol" sa nacita prvy retazec pravej strany pravidla
-            mnozina = new HashSet<>();
-            if (g.getTerminaly().contains(p.getPravaStrana().get(0))) { //ked je na pravej strane terminal, tak
-                mnozina.add(p.getPravaStrana().get(0));                 //sa tento terminal prida do "mnoziny"
-                if (vysledok.containsKey(symbol)) {                     // ak sa uz v mape nachadza prislusny kluc
-                    vysledok.get(symbol).add(p.getPravaStrana().get(0));    //tak sa k nemu prida "mnozina" obsahujuca terminal
-                } else                                                  //ak v mape neexistuje este kluc
-                    vysledok.put(p.getLavaStrana().get(0), mnozina);    //tak sa vytvori kluc a prida sa k nemu "mnozina"
-
-            } else if (p.getPravaStrana().get(0).equals(eps)) {         //ak je na pravej strane terminal
-                mnozina.add(eps);
-                if (vysledok.containsKey(symbol))                       //rovnakou podmienkou ako v predchadzajucom pripade
-                    vysledok.get(symbol).add(eps);                      //sa vlozi hodnota "epsilon" do vysledku
-                else
-                    vysledok.put(symbol, mnozina);
-            } }
-        for (Pravidlo p : g.getPravidla()) {
-            String symbol = p.getLavaStrana().get(0);
-            for (int i = 0; i < p.getPravaStrana().size(); i++) {
-                if (g.getNeterminaly().contains(p.getPravaStrana().get(0))) {  //ak je na pravej strane neterminal
-                    najdiMnozinu(p.getPravaStrana().get(0));
-                    if (mnozina.contains("epsilon")) {
-                        najdiMnozinu(p.getPravaStrana().get(i));
-                        mnozina.remove("epsilon");
-                   }
-
-                    //Iterator<String> itr = mnozina.iterator();
-
-                    // while(itr.hasNext()) {
-                    //System.out.println(mnozina);
-                    // System.out.println(itr.next());
-                    //if (!.getValue().contains(ele)) {
-                    //mnozina.add(ele);
-                    if (vysledok.containsKey(symbol)) {
-                        vysledok.get(symbol).addAll(mnozina);
-                    } else
-                        vysledok.put(symbol, mnozina);                      // tak sa do vysledku prida neterminal
-                    //}
-                }
+        for (String n : g.getNeterminaly()) {                   //Prehladavanie neterminalov
+            ArrayList<String> mnozina = new ArrayList<>();
+            vysledok.put(n, mnozina);                           //Pridanie neterminalov ako klucov do mapy
+        }
+        //Riesenie pravidiel tvaru A->aα, kedy "a" patri do First(A)
+        for (Pravidlo p : g.getPravidla()) {                    //Prehladavanie pravidiel
+            if (g.getTerminaly().contains(p.getPravaStrana().get(0))) {     //Ak je prvy retazec na pravej strane terminal,
+                vysledok.get(p.getLavaStrana().get(0)).add(p.getPravaStrana().get(0)); //prida sa do First neterminalu na lavej strane
             }
         }
-           /* for (Map.Entry<String, HashSet<String>> s : vysledok.entrySet()) {  //prehladavaju sa vysledky
-                System.out.println(vysledok.entrySet());
-                for (String neterminal : g.getNeterminaly()) {
-                    if (s.getValue().contains(neterminal)){
-                        najdiMnozinu(neterminal);
-                        System.out.println(mnozina);
-                        for(String ele: mnozina) {
-                            if (!s.getValue().contains(ele)) {
-                                mnozina.add(ele);
-                                if (vysledok.containsKey(s.getKey()))
-                                    vysledok.get(s.getKey()).add(ele.replace("[", "").replace("]", ""));
-                                else
-                                    vysledok.put(s.getKey(), mnozina);
-
+        //Riesenie pravidiel tvaru A->epsilon, kedy "epsilon" patri do First(A)
+        for (Pravidlo p : g.getPravidla()) {
+            if (p.getPravaStrana().get(0) == "epsilon") {       //Ak je na pravej strane "epsilon",
+                vysledok.get(p.getLavaStrana().get(0)).add("epsilon");   //prida sa do First neterminalu na lavej strane
+            }
+        }
+        //Riesenie pravidel tvaru A->Bα
+        boolean zmena = true;
+        while (zmena == true) { //cyklus sa opakuje pokial sa do niektorej z First mnozin prida novy prvok
+            zmena = false;
+            for (Pravidlo p : g.getPravidla()) {
+                boolean jeepsilon = false;
+                if (g.getNeterminaly().contains(p.getPravaStrana().get(0))) { //ked je prvy retazec na pravej strane neterminal,
+                    if (vysledok.get(p.getPravaStrana().get(0)).contains("epsilon")) { //zistujeme ci do jeho first mnoziny patri "epsilon"
+                        jeepsilon = true;
+                    } else {
+                        jeepsilon = false;
+                    }
+                    for (int i = 0; i < vysledok.get(p.getPravaStrana().get(0)).size(); i++) { //prehladame mnozinu first neterminalu na pravej strane
+                        if (vysledok.get(p.getLavaStrana().get(0)).contains(vysledok.get(p.getPravaStrana().get(0)).get(i))) {
+                            /*Ak mnozina first neterminalu na lavej strane pravidla uz obsahuje dany prvok mnoziny
+                            first neterminalu na pravej strane, tak sa nevykona nic */
+                        } else {                                                            //v opacnom pripade
+                            if (vysledok.get(p.getPravaStrana().get(0)).get(i) == "epsilon") { //overime, ci dany prvok je epsilon
+                            /*Ak nie je epsilon, tak dany prvok pridame do mnoziny first neterminalu na lavej strane pravidla
+                            a zaznamename, ze bola vykonana zmena (do first mnoziny bol pridany novy prvok) */
+                            } else {
+                                vysledok.get(p.getLavaStrana().get(0)).add(vysledok.get(p.getPravaStrana().get(0)).get(i));
+                                zmena = true;
                             }
                         }
                     }
-
-
-
-
-
                 }
-            }*/
+                for (int j = 1; j < p.getPravaStrana().size(); j++) { //prehladavame zvysne prvky pravej strany pravidla
+                    if (jeepsilon == true) {       // pre pripad, ze first mnozina predchadzajuceho neterminalu na pravej strane obsahuje epsilon
+                        if (g.getTerminaly().contains(p.getPravaStrana().get(j))) { //ak je nasledujuci prvok terminal
+                            if (vysledok.get(p.getLavaStrana().get(0)).contains(p.getPravaStrana().get(j))) {
 
+                            } else {        // a este nieje vo first mnozine neterminalu na lavej strane,
+                                vysledok.get(p.getLavaStrana().get(0)).add(p.getPravaStrana().get(j)); //tak ho tam pridame
+                                zmena = true;         //a zaznamename, ze bola vykonana zmena
+                            }
+                        }
+                        if (g.getNeterminaly().contains(p.getPravaStrana().get(j))) {  //ak je nasledujuci prvok neterminal
+                            if (vysledok.get(p.getPravaStrana().get(j)).contains("epsilon")) { //zistime, ci jeho first mnozina obsahuje epsilon
+                                jeepsilon = true;
+                            } else {
+                                jeepsilon = false;
+                            }
+                            for (int k = 0; k < vysledok.get(p.getPravaStrana().get(j)).size(); k++) { //prehladame first mnozinu tohto neterminalu
+                                if (vysledok.get(p.getLavaStrana().get(0)).contains(vysledok.get(p.getPravaStrana().get(j)).get(k))) {
 
+                                } else {                    // ak prvok z first mnoziny neterminalu este nieje v mnozine first neterminalu na lavej strane
+                                    if (vysledok.get(p.getPravaStrana().get(0)).get(k) == "epsilon") {
 
-        /*for(Map.Entry<String,HashSet<String>> s : vysledok.entrySet()){
-            for (String neterminal: g.getNeterminaly()) {
-                System.out.println(neterminal);
-                if (s.getValue().contains(neterminal)) {
-                    System.out.println(neterminal);
-                    System.out.println(s.getValue());
-
-                    //vysledok.remove(s.getKey(), s.getValue());
-                }
-            }
-
-        }*/
-        //vycistiVypis(g.getNeterminaly());
-    }
-
-        /*if (g.getTerminaly().contains(retazecFirst.get(0))) {
-                vysledok.add(retazecFirst.get(0));
-        } else {
-                if (g.getNeterminaly().contains(retazecFirst.get(0))) {
-                    for(Pravidlo p: g.getPravidla()){
-                        if (p.getLavaStrana().contains(retazecFirst.get(0))) {
-                            retazecFirst.clear();
-                            retazecFirst.add(p.getPravaStrana().get(0));
-                                if (g.getTerminaly().contains(retazecFirst.get(0))) {
-                                    vysledok.add(p.getPravaStrana().get(0));
-                                    g.getPravidla().iterator().next();
+                                    } else {                // a tento prvok nieje epsilon
+                                        vysledok.get(p.getLavaStrana().get(0)).add(vysledok.get(p.getPravaStrana().get(j)).get(k)); //tak ho pridame do first mnoziny neterminalu na lavej strane
+                                        zmena = true;                                       // a zaznamename, ze sa vykonala zmena
+                                    }
                                 }
+                            }
                         }
                     }
-                } else {
-                    if (retazecFirst.get(0).equals(eps)) {
-                        vysledok.add(retazecFirst.get(0));
-                    } else {
-                        throw new Exception("Prvy symbol sa nenachadza medzi terminalmi ani neterminalmi");
-                    }
-                }
-            }*/
-
-
-    // System.out.println(retazecFirst);
-
-
-    public void vypisMnozinu() {
-
-        for (Map.Entry<String, HashSet<String>> s : vysledok.entrySet()) {
-            System.out.println("First(" + s.getKey() + ") = " + s.getValue());
-        }
-        //vycistiVypis();
-    }
-
-    public HashSet<String> najdiMnozinu(String neterminal) {
-        for (Map.Entry<String, HashSet<String>> s : vysledok.entrySet()) {
-            if (s.getKey().equals(neterminal)) {
-                System.out.println(s.getValue().toString());
-                mnozina.addAll(s.getValue());
-
-            }
-        }
-
-        return mnozina;
-
-    }
-
-
-    /*public HashSet<String> iteruj(String neterminal) {
-        for (Map.Entry<String, HashSet<String>> s : vysledok.entrySet()) {
-            for (String ele : s.getValue()) {
-                if (neterminal.equals(ele)) {
-                    najdiMnozinu(neterminal);
-
-                }
-
-            }
-        }
-        return mnozina;
-    }*/
-
-    /*public void vycistiVypis() {
-        for (Map.Entry<String, HashSet<String>> s : vysledok.entrySet()) {
-            System.out.println(s.getKey());
-            pomocna = s.getValue();
-            System.out.println(pomocna);
-
-        }
-
-    }*/
-
-
-
-    /*private HashSet<String> terminaly;
-    private HashSet<String> neterminaly;
-    private String zaciatocnySymbol;
-    private LinkedHashSet<Pravidlo> pravidla;
-    ArrayList<ArrayList<String>> firstSet = new ArrayList<ArrayList<String>>();
-    ArrayList<String> firstInnerSet = new ArrayList<String>();*/
-
-    //ArrayList<String> firstSet = new ArrayList<String>();
-
-    // BezkontextovaGramatika BKgramatika = new BezkontextovaGramatika(terminaly, neterminaly, zaciatocnySymbol, pravidla);
-
-    /*public First() {
-        for (Pravidlo p : pravidla) {
-            ArrayList A = p.getLavaStrana();
-            for (int i = 0; i < p.getPravaStrana().size(); i++) {
-                if(terminaly.contains(p.getPravaStrana().get(i))){
-                  A.add(p.getPravaStrana().get(i));
-                  firstInnerSet.add(i,p.getPravaStrana().get(i));
                 }
             }
-   /* private HashSet<String> terminaly;
-    private HashSet<String> neterminaly;
-    private String zaciatocnySymbol;
-    private LinkedHashSet<Pravidlo> pravidla;
-
-    public HashSet First(HashSet<String> terminaly, HashSet<String> neterminaly, String zaciatocnySymbol, LinkedHashSet<Pravidlo> pravidla){
-        this.terminaly = terminaly;
-        this.neterminaly = neterminaly;
-        this.zaciatocnySymbol = zaciatocnySymbol;
-        for (Pravidlo p:pravidla){
-            for(terminaly.)
-
         }
-
- /*HashMap<String,HashSet<String>> kopia = (HashMap<String, HashSet<String>>) vysledok.clone();
-        System.out.println(neterminal);
-           for(Map.Entry<String,HashSet<String>> s : vysledok.entrySet()){
-               for(String ele: s.getValue()) {
-                   System.out.println(s.getKey());
-                   System.out.println(neterminal);
-                   System.out.println(ele);
-                   if (neterminal.equals(ele)) {
-                       System.out.println("okej");
-                       System.out.println(s.getKey());
-                       System.out.println(s.getValue());
-                       System.out.println(kopia);
-                       kopia.remove(s.getKey(),ele);
-                       System.out.println(kopia);
-                   }
-
-
-               }
-
-    } vysledok = kopia; System.out.println(kopia);return kopia;
-}*/
-
-
-
-   /* public ArrayList<String> getTerminaly(){
-        return terminaly;
+        return vysledok;
     }
-
-    public ArrayList<String> getNeterminaly(){
-        return neterminaly;
-    }
-
-    public String getZaciatocnySymbol(){
-        return zaciatocnySymbol;
-    }
-
-    public LinkedHashSet<Pravidlo> getPravidla(){
-        return pravidla;
-    }*/}
+}
 
 
--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -14,30 +14,23 @@ import java.util.LinkedHashSet;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        HashSet<String> neterminaly =new HashSet<>(Arrays.asList("S","E","T","F"));
-        HashSet<String> terminaly =new HashSet<>(Arrays.asList("$","+", "*","(",")","id"));
+
+        HashSet<String> neterminaly =new HashSet<>(Arrays.asList("S","A","B"));
+        HashSet<String> terminaly =new HashSet<>(Arrays.asList("a","b", "c"));
         String zaciatocnySymbol = "S";
-        FirstAFollow firstAFollow;
+        FirstAFollow firstAFollow = new FirstAFollow();
 
 
-        Pravidlo pravidlo1 = new Pravidlo(new ArrayList<>(Arrays.asList("S")), new ArrayList<>(Arrays.asList("E","$")));
-        Pravidlo pravidlo2 = new Pravidlo(new ArrayList<>(Arrays.asList("E")), new ArrayList<>(Arrays.asList("E","+","T")));
-        Pravidlo pravidlo3 = new Pravidlo(new ArrayList<>(Arrays.asList("E")), new ArrayList<>(Arrays.asList("T")));
-        Pravidlo pravidlo4 = new Pravidlo(new ArrayList<>(Arrays.asList("T")), new ArrayList<>(Arrays.asList("T","*","F")));
-        Pravidlo pravidlo5 = new Pravidlo(new ArrayList<>(Arrays.asList("T")), new ArrayList<>(Arrays.asList("F")));
-        Pravidlo pravidlo6 = new Pravidlo(new ArrayList<>(Arrays.asList("F")), new ArrayList<>(Arrays.asList("id")));
-        Pravidlo pravidlo7 = new Pravidlo(new ArrayList<>(Arrays.asList("F")), new ArrayList<>(Arrays.asList("(","E", ")")));
-        LinkedHashSet<Pravidlo> pravidla = new LinkedHashSet<>(Arrays.asList(pravidlo1, pravidlo2, pravidlo3, pravidlo4, pravidlo5, pravidlo6, pravidlo7));
-        BezkontextovaGramatika grammar = new BezkontextovaGramatika(terminaly,neterminaly,zaciatocnySymbol,pravidla) {
-        };
+        Pravidlo pravidlo1 = new Pravidlo(new ArrayList<>(Arrays.asList("S")), new ArrayList<>(Arrays.asList("A","B")));
+        Pravidlo pravidlo2 = new Pravidlo(new ArrayList<>(Arrays.asList("S")), new ArrayList<>(Arrays.asList("c","B")));
+        Pravidlo pravidlo3 = new Pravidlo(new ArrayList<>(Arrays.asList("A")), new ArrayList<>(Arrays.asList("a","A","c","B")));
+        Pravidlo pravidlo4 = new Pravidlo(new ArrayList<>(Arrays.asList("A")), new ArrayList<>(Arrays.asList("epsilon")));
+        Pravidlo pravidlo5 = new Pravidlo(new ArrayList<>(Arrays.asList("B")), new ArrayList<>(Arrays.asList("a","b")));
+        Pravidlo pravidlo6 = new Pravidlo(new ArrayList<>(Arrays.asList("B")), new ArrayList<>(Arrays.asList("A","c")));
+        LinkedHashSet<Pravidlo> pravidla = new LinkedHashSet<>(Arrays.asList(pravidlo1, pravidlo2, pravidlo3, pravidlo4, pravidlo5,pravidlo6));
+        BezkontextovaGramatika g = new BezkontextovaGramatika(terminaly,neterminaly,zaciatocnySymbol,pravidla);
 
-        //System.out.println(retazecFirst);
-        //System.out.println(pravidlo2.getLavaStrana());
-        //System.out.println(pravidlo2.getPravaStrana());
-        //System.out.println(pravidlo3.getPravaStrana());
-        firstAFollow = new FirstAFollow();
-        firstAFollow.first(grammar);
-        firstAFollow.vypisMnozinu();
+        firstAFollow.first(g);
 
     }}
 
