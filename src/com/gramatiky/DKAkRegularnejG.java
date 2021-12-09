6eb41c17e709a408e1f68b634881206e21e9570e
IvanAos <90446770+IvanAos@users.noreply.github.com>
2021-12-09 14:06:16 +0100

    Pridanie komentárov

--- src/com/gramatiky/DKAkRegularnejG.java
+++ src/com/gramatiky/DKAkRegularnejG.java
@@ -16,26 +16,33 @@ public class DKAkRegularnejG {
     public HashSet<String> symbolyDKAkRG;
     public String zaciatocnyStavDKAkRG;
     public Tabulka tabulkaDKAkRG;
+
     public DKAkRegularnejG(HashSet<String> terminaly, HashSet<String> neterminaly, String zaciatocnySymbol, LinkedHashSet<Pravidlo> pravidla) throws Exception {
        HashSet<String> akceptujuciStav = new HashSet<>();
-        Tabulka tabulka;
-        tabulka = new Tabulka();
-        int i = 1;
-        for (Pravidlo p : pravidla) {
+       Tabulka tabulka;
+       tabulka = new Tabulka();
+       int i = 1;
+       // Prehľadávanie pravidiel
+       for (Pravidlo p : pravidla) {
+           // Prípad keď má pravá strana len terminál
             if (p.getPravaStrana().size() == 1) {
                 if (terminaly.contains(p.getPravaStrana().get(0))) {
+
                     akceptujuciStav.add("qf" + i);
+
+                    // Prehľadanie akceptujúcich stavov
                     for (String c : akceptujuciStav) {
-
+                        // Zápis do tabuľky
                         if (c.contains("qf" + i)) {
-                            tabulka.pridajRiadok(p.getLavaStrana().get(0), p.getPravaStrana().get(0), c);
-                        }
 
+                            tabulka.pridajRiadok(p.getLavaStrana().get(0), p.getPravaStrana().get(0), c);
+
+                        }
                     }
                     i++;
                 }
             }
-
+            //Prípad keď má pravá strana terminál a neterminál
             if (p.getPravaStrana().size() == 2) {
                 if (terminaly.contains(p.getPravaStrana().get(0)) & neterminaly.contains(p.getPravaStrana().get(1))) {
 
@@ -43,17 +50,15 @@ public class DKAkRegularnejG {
 
                 }
             }
-
-        }
+       }
         this.akceptujuciStavDKAkRG=akceptujuciStav;
         this.zaciatocnyStavDKAkRG = zaciatocnySymbol;
         this.tabulkaDKAkRG=tabulka;
         this.stavyDKAkRG=neterminaly;
         this.symbolyDKAkRG=terminaly;
-        tabulka.vypisTabulku();
-        System.out.println(akceptujuciStavDKAkRG);
-        System.out.println(zaciatocnyStavDKAkRG);
+
     }
+    // Metódy, ktoré vracajú parametre potrebné na konštrukciu automatu
     public HashSet<String> vratAkceptujuceStavyDKAkRG(){return akceptujuciStavDKAkRG;}
     public HashSet<String> vratStavyDKAkRG(){return stavyDKAkRG;}
     public HashSet<String> vratSymbolyDKAkRG(){return symbolyDKAkRG;}
